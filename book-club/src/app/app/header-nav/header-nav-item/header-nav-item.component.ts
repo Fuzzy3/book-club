4f55798cbe00dae268197c9540b2f845863d1ef0
Søren Oest Balmer <soj@systematic.com>
2021-10-01 15:33:20 +0200

    routes

---
+++ book-club/src/app/app/header-nav/header-nav-item/header-nav-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header-nav-item',
+  templateUrl: './header-nav-item.component.html',
+  styleUrls: ['./header-nav-item.component.scss']
+})
+export class HeaderNavItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
