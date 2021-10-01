951d333378ac1e47e256c667e37ad5fb79d14e7b
Søren Oest Balmer <soj@systematic.com>
2021-10-01 15:32:34 +0200

    Routes

--- book-club/src/app/app.module.ts
+++ book-club/src/app/app.module.ts
@@ -2,13 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './app/header/header.component';
+import { HeaderNavComponent } from './app/header-nav/header-nav.component';
+import { HeaderNavItemComponent } from './app/header-nav/header-nav-item/header-nav-item.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeaderNavComponent,
+    HeaderNavItemComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    RouterModule
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
